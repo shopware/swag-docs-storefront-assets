398bc8decfa78a471835764e4b9625326fd16732
Dennis Garding <di@shopware.com>
2020-08-27 09:17:44 +0200

    NEXT-10072 - Plugin compatibility

--- tests/UsedClassesAvailableTest.php
+++ tests/UsedClassesAvailableTest.php
@@ -14,8 +14,13 @@ class UsedClassesAvailableTest extends TestCase
     {
         $namespace = str_replace('Tests', '', __NAMESPACE__);
 
-        foreach ($this->getPluginClasses() as $class) {
-            $classRelativePath = str_replace(['.php', '/'], ['', '\\'], $class->getRelativePathname());
+        $files = $this->getPluginClasses();
+        foreach ($files as $file) {
+            if (!preg_match('/.*.php$/', $file->getRelativePathname())) {
+                continue;
+            }
+
+            $classRelativePath = str_replace(['.php', '/'], ['', '\\'], $file->getRelativePathname());
 
             $this->getMockBuilder($namespace . '\\' . $classRelativePath)
                 ->disableOriginalConstructor()
@@ -23,14 +28,14 @@ class UsedClassesAvailableTest extends TestCase
         }
 
         // Nothing broke so far, classes seem to be instantiable
-        $this->assertTrue(true);
+        static::assertCount(5, $files);
     }
 
     private function getPluginClasses(): Finder
     {
         $finder = new Finder();
-        $finder->in(realpath(__DIR__ . '/../'));
+        $finder->in(realpath(__DIR__ . '/../src'));
         $finder->exclude('Test');
-        return $finder->files()->name('*.php');
+        return $finder->files()->name('/.*.(php|js|scss)$/');
     }
 }
